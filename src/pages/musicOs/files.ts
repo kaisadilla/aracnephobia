7705111f56e89b3b9ae1732595fdcc54209f0f14
kaisadilla <58343029+kaisadilla@users.noreply.github.com>
2026-08-22 21:02:11 +0200

    Music OS part XII — Pre-release III

--- src/pages/musicOs/files.ts
+++ src/pages/musicOs/files.ts
@@ -3,10 +3,38 @@ import i_err_mask from 'assets/img/music_os/error/mask.png';
 import i_err_rage from 'assets/img/music_os/error/rage.png';
 import i_err_rope from 'assets/img/music_os/error/rope.png';
 
-import i_mask_p0 from 'assets/img/music_os/gallery/the_mask/photos/test1.jpg';
-import i_mask_p1 from 'assets/img/music_os/gallery/the_mask/photos/test2.jpg';
-import i_mask_p2 from 'assets/img/music_os/gallery/the_mask/photos/test3.jpg';
-import i_mask_p3 from 'assets/img/music_os/gallery/the_mask/photos/test4.gif';
+import i_mask_p0 from 'assets/img/music_os/gallery/the_mask/photos/0.jpg';
+import i_mask_p1 from 'assets/img/music_os/gallery/the_mask/photos/1.jpg';
+import i_mask_p2 from 'assets/img/music_os/gallery/the_mask/photos/2.jpg';
+import i_mask_p3 from 'assets/img/music_os/gallery/the_mask/photos/3.jpg';
+import i_mask_p4 from 'assets/img/music_os/gallery/the_mask/photos/4.jpg';
+import i_mask_p5 from 'assets/img/music_os/gallery/the_mask/photos/5.jpg';
+import i_mask_p6 from 'assets/img/music_os/gallery/the_mask/photos/6.jpg';
+
+import i_mask_d0 from 'assets/img/music_os/gallery/the_mask/drawings/0.jpg';
+import i_mask_d1 from 'assets/img/music_os/gallery/the_mask/drawings/1.jpg';
+import i_mask_d2 from 'assets/img/music_os/gallery/the_mask/drawings/2.jpg';
+import i_mask_d3 from 'assets/img/music_os/gallery/the_mask/drawings/3.jpg';
+import i_mask_d4 from 'assets/img/music_os/gallery/the_mask/drawings/4.jpg';
+import i_mask_d5 from 'assets/img/music_os/gallery/the_mask/drawings/5.jpg';
+import i_mask_d6 from 'assets/img/music_os/gallery/the_mask/drawings/6.jpg';
+import i_mask_d7 from 'assets/img/music_os/gallery/the_mask/drawings/7.jpg';
+import i_mask_d8 from 'assets/img/music_os/gallery/the_mask/drawings/8.jpg';
+
+import i_leak_p0 from 'assets/img/music_os/gallery/the_leak/photos/0.jpg';
+import i_leak_p1 from 'assets/img/music_os/gallery/the_leak/photos/1.jpg';
+import i_leak_p2 from 'assets/img/music_os/gallery/the_leak/photos/2.jpg';
+import i_leak_p3 from 'assets/img/music_os/gallery/the_leak/photos/3.jpg';
+import i_leak_p4 from 'assets/img/music_os/gallery/the_leak/photos/4.jpg';
+import i_leak_p5 from 'assets/img/music_os/gallery/the_leak/photos/5.jpg';
+
+import i_leak_d0 from 'assets/img/music_os/gallery/the_leak/drawings/0.jpg';
+import i_leak_d1 from 'assets/img/music_os/gallery/the_leak/drawings/1.jpg';
+import i_leak_d2 from 'assets/img/music_os/gallery/the_leak/drawings/2.jpg';
+import i_leak_d3 from 'assets/img/music_os/gallery/the_leak/drawings/3.jpg';
+import i_leak_d4 from 'assets/img/music_os/gallery/the_leak/drawings/4.jpg';
+import i_leak_d5 from 'assets/img/music_os/gallery/the_leak/drawings/5.jpg';
+import i_leak_d6 from 'assets/img/music_os/gallery/the_leak/drawings/6.jpg';
 
 export interface BrokenFile {
   type: 'broken';
@@ -183,27 +211,6 @@ function other (
   }
 }
 
-export const THE_MASK = folder(
-  "THE//MASK",
-  [
-    song("the_mask", "Happy Happy Happy.mp3", "happy_happy_happy"),
-    song("the_mask", "Tomorrow.mp3", "tomorrow"),
-    song("the_mask", "Too many days.mp3", "too_many_days"),
-    song("the_mask", "Clean Clean Clean.mp3", "clean_clean_clean"),
-  ],
-  [
-    picture("Test 1.png", i_mask_p0),
-    picture("This is an incredibly long title for a picture.JPG", i_mask_p1),
-    picture("EW.pdf", i_mask_p2),
-    picture("Watching.gif", i_mask_p3),
-  ],
-  [
-
-  ],
-  [
-  ]
-);
-
 export const THE_LEAK = folder(
   "THE//LEAK",
   [
@@ -213,16 +220,21 @@ export const THE_LEAK = folder(
     song("the_leak", "Restless.mp3", "restless"),
   ],
   [
-    picture("Test 1.png", i_mask_p0),
-    picture("This is an incredibly long title for a picture.JPG", i_mask_p1),
-    picture("EW.pdf", i_mask_p2),
-    picture("Watching.gif", i_mask_p3),
+    picture("Burned.jpg", i_leak_p0),
+    picture("The morning before.jpg", i_leak_p1),
+    picture("Something underneath.jpg", i_leak_p2),
+    picture("Not saved.jpg", i_leak_p3),
+    picture("Not pure.jpg", i_leak_p4),
+    picture("I was bright once.jpg", i_leak_p5),
   ],
   [
-    picture("Test 1.png", i_mask_p0),
-    picture("This is an incredibly long title for a picture.JPG", i_mask_p1),
-    picture("EW.pdf", i_mask_p2),
-    picture("Watching.gif", i_mask_p3),
+    picture("Lost.png", i_leak_d0),
+    picture("Not the right place.png", i_leak_d1),
+    picture("You can still breathe.png", i_leak_d2),
+    picture("I need to see the real world.png", i_leak_d3),
+    picture("Not chosen to rise.png", i_leak_d4),
+    picture("I survived everything.png", i_leak_d5),
+    picture("Gnorw.png", i_leak_d6),
   ],
   [
     link(
@@ -234,6 +246,36 @@ export const THE_LEAK = folder(
   true
 );
 
+export const THE_MASK = folder(
+  "THE//MASK",
+  [
+    song("the_mask", "Happy Happy Happy.mp3", "happy_happy_happy"),
+    song("the_mask", "Tomorrow.mp3", "tomorrow"),
+    song("the_mask", "Too many days.mp3", "too_many_days"),
+    song("the_mask", "Clean Clean Clean.mp3", "clean_clean_clean"),
+  ],
+  [
+    picture("Dopamine disaster!.jpg", i_mask_p0),
+    picture("Everyday is Halloween.jpg", i_mask_p1),
+    picture("Sunshine.jpg", i_mask_p2),
+    picture("Me with friends.jpg", i_mask_p3),
+    picture("Easy to love.jpg", i_mask_p4),
+    picture("Lonely.jpg", i_mask_p5),
+    picture("Camoff.jpg", i_mask_p6),
+  ],
+  [
+    picture("Dreaming of.png", i_mask_d0),
+    picture("Starting glowing.png", i_mask_d1),
+    picture("How's starting.png", i_mask_d2),
+    picture("Jump.png", i_mask_d3),
+    picture("Sugar wired electricity.png", i_mask_d4),
+    picture("Nothing.png", i_mask_d5),
+    picture("Inside.png", i_mask_d6),
+    picture("My real friends.png", i_mask_d7),
+    picture("The Beginning.png", i_mask_d8),
+  ],
+  []
+);
 
 const FILES = [
   THE_MASK,
